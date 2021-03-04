846845773be722002235d63ac607f4f981508a6f
Josh Klauer <jklauer@uwm.edu>
2021-03-04 10:11:55 -0600

    This gives the signing key for the project

--- app/src/main/java/com/example/nutritiontrackerguiv4/MainActivity.java
+++ app/src/main/java/com/example/nutritiontrackerguiv4/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nutritiontrackerguiv4;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
