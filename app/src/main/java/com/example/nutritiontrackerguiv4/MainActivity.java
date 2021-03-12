f93e1c9ff0283aa4f074a58d915d2e0a34678c51
ShapedSilver <EricTorres346@gmail.com>
2021-03-11 18:58:42 -0600

    Created first iteration of the method that reads the meal info. Only does calories right now.

--- app/src/main/java/com/example/nutritiontrackerguiv4/MainActivity.java
+++ app/src/main/java/com/example/nutritiontrackerguiv4/MainActivity.java
@@ -12,8 +12,15 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,6 +55,40 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    /*Helper method extractNums reads the number from a line that contains other text.
+     Argument: String s, the string that contains the number
+     Retuens the number as an integer.
+     */
+    public int extractNums(String s) {
+        s = s.replaceAll("[^\\d]", " ");
+        s = s.trim();
+        return Integer.parseInt(s);
+    }
+
+    //Prototype method for reading values from userMealData. Right now it only returns calories,
+    //but as we have a clearer idea of what is all being stored, the array size can be increased
+    //and the logic that extracts the numbers can be repeated.
+    //Returns an array of numbers that will be output onto the dashboard.
+    public int[] readMeals() {
+        File mealsPage = new File(getFilesDir(), "userMealData.txt");
+        int calorie_count = 0;
+        int[] info = new int[1];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(mealsPage));
+            String line = br.readLine();
+            while (line != null){
+                if (line.contains("Meal Calories")) {
+                    calorie_count += extractNums(line);
+                    info[0] = calorie_count;
+                }
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return info;
     }
 
 
