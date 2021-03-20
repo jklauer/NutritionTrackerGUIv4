efa14a8cad7927f4f0b1e7a3db75d4e00151712c
ShapedSilver <EricTorres346@gmail.com>
2021-03-20 10:29:52 -0500

    Added code to keep two instances of the database from being created for one device.

--- app/src/main/java/com/example/nutritiontrackerguiv4/database/NutritionDatabase.java
+++ app/src/main/java/com/example/nutritiontrackerguiv4/database/NutritionDatabase.java
@@ -1,6 +1,9 @@
 package com.example.nutritiontrackerguiv4.database;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = { Allergies.class, Day.class, Ingredient.class, Meal.class,
@@ -15,5 +18,20 @@ public abstract class NutritionDatabase extends RoomDatabase {
     public abstract Recipe_Ingredient_JoinDAO getRecipeIngredientJoinDAO();
     public abstract RecipeDAO getRecipeDAO();
     public abstract UserDAO getUserDAO();
+
+    private static NutritionDatabase INSTANCE;
+
+    public static NutritionDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (NutritionDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NutritionDatabase.class, "nutrition_database")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 
 }
