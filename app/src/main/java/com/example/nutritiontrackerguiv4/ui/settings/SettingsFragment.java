fdf240b46ec54a39cb19d79469362a5eb9402e12
Josh Klauer <jklauer@uwm.edu>
2021-03-26 20:07:04 -0500

    code to print out an xls file is in the SettingsFragment.java

--- app/src/main/java/com/example/nutritiontrackerguiv4/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/example/nutritiontrackerguiv4/ui/settings/SettingsFragment.java
@@ -1,11 +1,14 @@
 package com.example.nutritiontrackerguiv4.ui.settings;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,6 +17,25 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.nutritiontrackerguiv4.Notifications.CreateNotification;
 import com.example.nutritiontrackerguiv4.R;
 import com.example.nutritiontrackerguiv4.StartPage;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
 
 public class SettingsFragment extends Fragment {
 
@@ -50,7 +72,50 @@ public class SettingsFragment extends Fragment {
         });
 
 
+        try{InputStream myInput;
+            // initialize asset manager
+            AssetManager assetManager = getActivity().getAssets();
+            //  open excel sheet
+            myInput = assetManager.open("FOOD.xls");
+            // Create a POI File System object
+            POIFSFileSystem myFileSystem = new POIFSFileSystem(myInput);
+            // Create a workbook using the File System
+            HSSFWorkbook myWorkBook = new HSSFWorkbook(myFileSystem);
+            // Get the first sheet from workbook
+            HSSFSheet mySheet = myWorkBook.getSheetAt(0);
+            // We now need something to iterate through the cells.
+            Iterator<Row> rowIter = mySheet.rowIterator();
+            int rowno =0;
 
+            while (rowIter.hasNext()) {
+                System.out.println(rowno);
+                //Log.e(TAG, " row no "+ rowno );
+                HSSFRow myRow = (HSSFRow) rowIter.next();
+                if(rowno !=0) {
+                    Iterator<Cell> cellIter = myRow.cellIterator();
+                    int colno =0;
+                    String sno="", date="", det="";
+                    while (cellIter.hasNext()) {
+                        HSSFCell myCell = (HSSFCell) cellIter.next();
+                        if (colno==0){
+                            sno = myCell.toString();
+                        }else if (colno==1){
+                            date = myCell.toString();
+                        }else if (colno==2){
+                            det = myCell.toString();
+                        }
+                        colno++;
+                        System.out.println(myCell.toString());
+                        //Log.e(TAG, " Index :" + myCell.getColumnIndex() + " -- " + myCell.toString());
+                    }
+                    //textView.append( sno + " -- "+ date+ "  -- "+ det+"\n");
+                }
+                rowno++;
+            }
+        } catch (Exception e) {
+            //Log.e(TAG, "error "+ e.toString());
+            System.out.println(e);
+        }
 
         return root;
     }
