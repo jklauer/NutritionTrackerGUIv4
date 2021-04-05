80c8edda1fc029ca975a3f15fcc443dd4301b93c
ShapedSilver <EricTorres346@gmail.com>
2021-04-05 15:28:03 -0500

    Created a graph for the calories for the last seven days

--- app/src/main/java/com/example/nutritiontrackerguiv4/ui/Tracking/TrackingActivity.java
+++ app/src/main/java/com/example/nutritiontrackerguiv4/ui/Tracking/TrackingActivity.java
@@ -2,11 +2,19 @@ package com.example.nutritiontrackerguiv4.ui.Tracking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import java.util.ArrayList;
 
 import com.example.nutritiontrackerguiv4.R;
 import com.example.nutritiontrackerguiv4.database.NutritionDatabase;
@@ -14,12 +22,24 @@ import com.example.nutritiontrackerguiv4.database.NutritionDatabase;
 public class TrackingActivity extends AppCompatActivity {
 
     NutritionDatabase db;
+    ArrayList lineEntries;
+    LineData lineData;
+    LineDataSet lineDataSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracking);
         this.db = NutritionDatabase.getDatabase(getApplicationContext());
+
+        LineChart lineChart = findViewById(R.id.lineChart);
+        getEntries();
+        lineDataSet = new LineDataSet(lineEntries, "Calories for the last seven days");
+        lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+        lineDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        lineDataSet.setValueTextColor(Color.BLACK);
+        lineDataSet.setValueTextSize(18f);
 
         TextView calView = findViewById(R.id.averageCal);
         TextView vitAView = findViewById(R.id.averageVitA);
@@ -33,6 +53,31 @@ public class TrackingActivity extends AppCompatActivity {
         calView.setText("Average Calories: " + averageCal);
         vitAView.setText("Average Vitamin A: " + averageVitA);
         vitCView.setText("Average Vitamin C: " + averageVitC);
+    }
+
+    private int getCalorieHelper(String day)
+    {
+        int cals = 0;
+        try
+        {
+            cals = db.getIngredientDAO().findCaloriesOnDay(day).get(0);
+        }catch (NullPointerException e)
+        {
+            System.out.println("No calories that day");
+        }
+        return cals;
+    }
+
+    private void getEntries() {
+        ArrayList<String> lastSeven = lastSevenDays();
+        lineEntries = new ArrayList<>();
+        lineEntries.add(new Entry(1, getCalorieHelper(lastSeven.get(0))));
+        lineEntries.add(new Entry(2, getCalorieHelper(lastSeven.get(1))));
+        lineEntries.add(new Entry(3, getCalorieHelper(lastSeven.get(2))));
+        lineEntries.add(new Entry(4, getCalorieHelper(lastSeven.get(3))));
+        lineEntries.add(new Entry(5, getCalorieHelper(lastSeven.get(4))));
+        lineEntries.add(new Entry(6, getCalorieHelper(lastSeven.get(5))));
+        lineEntries.add(new Entry(7, getCalorieHelper(lastSeven.get(6))));
     }
 
     //Returns the average calories, vitamin A, and vitamin C, in that order
