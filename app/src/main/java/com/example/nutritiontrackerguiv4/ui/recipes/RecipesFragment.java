b624a90f5d24387f1bc5cb80a90b7fe0992a1254
Elliott <etwink@uwm.edu>
2021-04-24 19:02:45 -0500

    updated recipes to be a fragment

---
+++ app/src/main/java/com/example/nutritiontrackerguiv4/ui/recipes/RecipesFragment.java
@@ -0,0 +1,38 @@
+package com.example.nutritiontrackerguiv4.ui.recipes;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nutritiontrackerguiv4.R;
+
+public class RecipesFragment extends Fragment {
+
+    private RecipesViewModel mViewModel;
+
+    public static RecipesFragment newInstance() {
+        return new RecipesFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.recipes_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(RecipesViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/nutritiontrackerguiv4/ui/recipes/RecipesViewModel.java
@@ -0,0 +1,7 @@
+package com.example.nutritiontrackerguiv4.ui.recipes;
+
+import androidx.lifecycle.ViewModel;
+
+public class RecipesViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
