c12eb2fceb36862254fa22f93a2862bc2c8bdcf6
Elliott <etwink@uwm.edu>
2021-04-26 03:54:01 -0500

    Added image files and updated the recipes pages to use them. Also changed the picture for the recipes bottom nav bar.

--- app/src/main/java/com/example/nutritiontrackerguiv4/ui/recipes/RecipesViewModel.java
+++ app/src/main/java/com/example/nutritiontrackerguiv4/ui/recipes/RecipesViewModel.java
@@ -1,7 +1,18 @@
 package com.example.nutritiontrackerguiv4.ui.recipes;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class RecipesViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<String> mText;
+
+    public RecipesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is recipes fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
